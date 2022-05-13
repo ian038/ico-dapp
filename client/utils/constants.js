083e49c28e0eec61852b4b7b3771e9a581377b7e
ian038 <ianphua3@gmail.com>
2022-05-12 20:34:39 -0400

    Added constants and changed some styling

---
+++ client/utils/constants.js
@@ -0,0 +1,7 @@
+import trippyDudesAbi from './TrippyDudes.json'
+import trippyDudesTokenAbi from './TrippyDudesToken.json'
+
+export const trippyDudesAddress = '0x315d812Eb76Fac89E88B2c5a6bEAeFFbC1fE4652'
+export const tdAbi = trippyDudesAbi.abi
+export const trippyDudesTokenAddress = '0xEB6907FB6E4F282bB3e348384b94cE1E09513696'
+export const tdtAbi = trippyDudesTokenAbi.abi
